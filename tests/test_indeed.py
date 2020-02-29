56151982304848df0a030464181701e5b54a76f5
dli <davidli012345@gmail.com>
2020-02-28 20:03:51 -0800

    Updating utils

--- tests/test_indeed.py
+++ tests/test_indeed.py
@@ -1,7 +1,10 @@
 from scrapper.indeed import get_num_jobs
 from scrapper.util import get_indeed_url
-
-fake_args = {'email_from': 'a', 'email_password': 'a', 'email_to': 'a', 'input_job': 'Developer', 'input_quote': False, 'input_city': 'Victoria', 'input_state': 'BC', 'testing': False}
+import argparse
+fake_args = argparse.Namespace()
+fake_args.input_city = 'Victoria'
+fake_args.input_state = 'BC'
+fake_args.input_quote = False
 def test_get_num_jobs():
     indeed_url = get_indeed_url(fake_args)
     num_jobs = get_num_jobs(indeed_url)
