3d8721d322c3e39c16453f7de7a0fa2b36d3fa54
dli <davidli012345@gmail.com>
2020-02-28 20:15:26 -0800

    adding failure case

--- scrapper/__init__.py
+++ scrapper/__init__.py
@@ -1,6 +1,1 @@
 from .indeed import get_num_jobs
-def smile():
-    return ":)"
-
-def frown():
-    return ":("
--- tests/test_indeed.py
+++ tests/test_indeed.py
@@ -1,13 +1,26 @@
 from scrapper.indeed import get_num_jobs
 from scrapper.util import get_indeed_url
 import argparse
-fake_args = argparse.Namespace()
-fake_args.input_city = 'Victoria'
-fake_args.input_state = 'BC'
-fake_args.input_quote = False
-fake_args.input_job = 'Software Developer'
+
+def make_namespace(job):
+  fake_args = argparse.Namespace()
+  fake_args.input_city = 'Victoria'
+  fake_args.input_state = 'BC'
+  fake_args.input_quote = False
+  fake_args.input_job = job
+  return fake_args
+
 def test_get_num_jobs():
-    indeed_url = get_indeed_url(fake_args)
+    indeed_url = get_indeed_url(make_namespace('Software Developer'))
     num_jobs = get_num_jobs(indeed_url)
     print(num_jobs)
     assert num_jobs > 0
+
+
+def test_get_num_jobs_fail():
+    indeed_url = get_indeed_url(make_namespace('Sofware developer king god monkey master'))
+    try:
+      num_jobs = get_num_jobs(indeed_url)
+      assert False
+    except Exception as e:
+      assert True
